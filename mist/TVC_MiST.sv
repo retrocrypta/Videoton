c9a027a704b6d9553f86eeee830a9d4f98507a2a
Gyorgy Szombathelyi <8644936+gyurco@users.noreply.github.com>
2023-10-28 23:34:55 +0200

    Add SiDi/SiDi128 ports

--- mist/TVC_MiST.sv
+++ mist/TVC_MiST.sv
@@ -1,20 +1,31 @@
 module TVC_MiST(
+	input         CLOCK_27,
+`ifdef USE_CLOCK_50
+	input         CLOCK_50,
+`endif
+
 	output        LED,
-	output  [5:0] VGA_R,
-	output  [5:0] VGA_G,
-	output  [5:0] VGA_B,
+	output [VGA_BITS-1:0] VGA_R,
+	output [VGA_BITS-1:0] VGA_G,
+	output [VGA_BITS-1:0] VGA_B,
 	output        VGA_HS,
 	output        VGA_VS,
-	output        AUDIO_L,
-	output        AUDIO_R,
+
 	input         SPI_SCK,
 	inout         SPI_DO,
 	input         SPI_DI,
-	input         SPI_SS2,
-	input         SPI_SS3,
+	input         SPI_SS2,    // data_io
+	input         SPI_SS3,    // OSD
+	input         CONF_DATA0, // SPI_SS for user_io
+
+`ifdef USE_QSPI
+	input         QSCK,
+	input         QCSn,
+	inout   [3:0] QDAT,
+`endif
+`ifndef NO_DIRECT_UPLOAD
 	input         SPI_SS4,
-	input         CONF_DATA0,
-	input         CLOCK_27,
+`endif
 
 	output [12:0] SDRAM_A,
 	inout  [15:0] SDRAM_DQ,
@@ -26,20 +37,94 @@ module TVC_MiST(
 	output        SDRAM_nCS,
 	output  [1:0] SDRAM_BA,
 	output        SDRAM_CLK,
-	output        SDRAM_CKE
+	output        SDRAM_CKE,
+
+`ifdef DUAL_SDRAM
+	output [12:0] SDRAM2_A,
+	inout  [15:0] SDRAM2_DQ,
+	output        SDRAM2_DQML,
+	output        SDRAM2_DQMH,
+	output        SDRAM2_nWE,
+	output        SDRAM2_nCAS,
+	output        SDRAM2_nRAS,
+	output        SDRAM2_nCS,
+	output  [1:0] SDRAM2_BA,
+	output        SDRAM2_CLK,
+	output        SDRAM2_CKE,
+`endif
+
+	output        AUDIO_L,
+	output        AUDIO_R,
+`ifdef I2S_AUDIO
+	output        I2S_BCK,
+	output        I2S_LRCK,
+	output        I2S_DATA,
+`endif
+`ifdef USE_AUDIO_IN
+	input         AUDIO_IN,
+`endif
+	input         UART_RX,
+	output        UART_TX
+
 );
 
-`include "build_id.v" 
+`ifdef NO_DIRECT_UPLOAD
+localparam bit DIRECT_UPLOAD = 0;
+wire SPI_SS4 = 1;
+`else
+localparam bit DIRECT_UPLOAD = 1;
+`endif
+
+`ifdef USE_QSPI
+localparam bit QSPI = 1;
+assign QDAT = 4'hZ;
+`else
+localparam bit QSPI = 0;
+`endif
+
+`ifdef VGA_8BIT
+localparam VGA_BITS = 8;
+`else
+localparam VGA_BITS = 6;
+`endif
+
+`ifdef BIG_OSD
+localparam bit BIG_OSD = 1;
+localparam SEP = "-;";
+`else
+localparam bit BIG_OSD = 0;
+localparam SEP = "";
+`endif
+
+// remove this if the 2nd chip is actually used
+`ifdef DUAL_SDRAM
+assign SDRAM2_A = 13'hZZZZ;
+assign SDRAM2_BA = 0;
+assign SDRAM2_DQML = 0;
+assign SDRAM2_DQMH = 0;
+assign SDRAM2_CKE = 0;
+assign SDRAM2_CLK = 0;
+assign SDRAM2_nCS = 1;
+assign SDRAM2_DQ = 16'hZZZZ;
+assign SDRAM2_nCAS = 1;
+assign SDRAM2_nRAS = 1;
+assign SDRAM2_nWE = 1;
+`endif
+
+`include "build_id.v"
 
 localparam CONF_STR = {
 	"TVC;CAS;",
 	"F,ROMCRT,Load Cartridge;",
 	"S0U,DSK,Mount Drive A;",
 	"S1U,DSK,Mount Drive B;",
+	SEP,
 	"O23,Scandoubler Fx,None,CRT 25%,CRT 50%,CRT 75%;",
+	SEP,
 	"O45,CPU Speed,3.125MHz,6.25MHz,12.5MHz;",
 	"O6,Joystick Swap,Off,On;",
-//	"O7,Composite Blend,Off,On;",
+	"O7,Composite Blend,Off,On;",
+	SEP,
 	"T1,Cart Unload;",
 	"T0,Reset;",
 	"V,v1.0.",`BUILD_DATE
@@ -88,7 +173,8 @@ wire [31:0] img_size;
 
 user_io #(
 	.STRLEN(($size(CONF_STR)>>3)),
-	.ROM_DIRECT_UPLOAD(1'b0))
+	.ROM_DIRECT_UPLOAD(1'b0),
+	.FEATURES(32'h0 | (BIG_OSD << 13)))
 user_io(
 	.clk_sys        (CLK_50M        ),
 	.conf_str       (CONF_STR       ),
@@ -227,14 +313,16 @@ tvctop tvctop (
 	.SDRAM_CKE(SDRAM_CKE)
 );
 
-mist_video #(.COLOR_DEPTH(6), .SD_HCNT_WIDTH(12)) mist_video(
+mist_video #(.COLOR_DEPTH(6), .SD_HCNT_WIDTH(12), .USE_BLANKS(1'b1), .OUT_COLOR_DEPTH(VGA_BITS), .BIG_OSD(BIG_OSD)) mist_video(
 	.clk_sys        ( CLK_50M          ),
 	.SPI_SCK        ( SPI_SCK          ),
 	.SPI_SS3        ( SPI_SS3          ),
 	.SPI_DI         ( SPI_DI           ),
-	.R              ( blankn ? R : 0   ),
-	.G              ( blankn ? G : 0   ),
-	.B              ( blankn ? B : 0   ),
+	.R              ( R                ),
+	.G              ( G                ),
+	.B              ( B                ),
+	.HBlank         ( HBlank           ),
+	.VBlank         ( VBlank           ),
 	.HSync          ( HSync            ),
 	.VSync          ( VSync            ),
 	.VGA_R          ( VGA_R            ),
